4b72901b080875a247cc277a775ae242b269fe5f
pranay1260 <143398234+pranay1260@users.noreply.github.com>
2026-04-11 16:09:29 +0530

    first commit

---
+++ src/main/java/com/ration/ration_system/RationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ration.ration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RationSystemApplication.class, args);
+	}
+
+}
